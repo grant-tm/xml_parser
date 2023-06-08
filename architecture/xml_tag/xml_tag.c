044963df183446760a92c21ece8aba5fe61133d5
grant.tm <grant.t.morgan@gmail.com>
2023-06-08 16:41:54 -0700

    methods for getting/setting tag name

--- architecture/xml_tag/xml_tag.c
+++ architecture/xml_tag/xml_tag.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "xml_attribute_set.h"
+#include "../xml_attribute_list/xml_attribute_list.h"
 #include "xml_tag.h"
+
+#define MAX_NAME_LENGTH (size_t) 127
 
 typedef struct xml_tag {
     char* name;
-    xml_attribute_set* attributes;
-};
+    xml_attribute_list* attributes;
+} xml_tag;
 
 /* CREATE, INITIALIZE, DESTROY */
 xml_tag* xml_tag_create(){
@@ -25,6 +27,35 @@ void xml_tag_destroy(xml_tag* tag){
 }
 
 /* GETTERS */
-
+char* xml_tag_get_name(xml_tag* tag){
+    if(tag->name){
+        return tag->name;
+    }
+    return NULL;
+}
 
 /* SETTERS */
+xml_tag* xml_tag_set_name(xml_tag* tag, char* new_name){
+    
+    if(!new_name){
+        return NULL;
+    }
+
+    size_t new_size;
+    if(strlen(new_name) >= MAX_NAME_LENGTH)
+        new_size = MAX_NAME_LENGTH;
+    else
+        new_size = strlen(new_name)+1;
+
+    char* successful_resize = (char*) realloc(tag->name, new_size);
+    if(!successful_resize){
+        fprintf(stderr, "Error: tag name resize failed.\n");
+        return NULL;
+    }
+    tag->name = successful_resize;
+    memset(tag->name, '\0', new_size);
+    strncpy(tag->name, new_name, new_size-1);
+    return tag;
+}
+
+
--- architecture/xml_tag/xml_tag.h
+++ architecture/xml_tag/xml_tag.h
@@ -1,10 +1,14 @@
 #ifndef _XML_tag_H
 #define _XML_tag_H
 
-extern struct xml_tag;
+typedef struct xml_tag xml_tag;
 
 xml_tag* xml_tag_create();
 xml_tag* xml_tag_initialize();
 void xml_tag_destroy();
+
+char* xml_tag_get_name(xml_tag* tag);
+
+xml_tag* xml_tag_set_name(xml_tag* tag, char* new_name);
 
 #endif
