f1f706eafcf7e2e2d44e925cd40c2f123ef90bc7
grant.tm <grant.t.morgan@gmail.com>
2023-06-08 01:24:33 -0700

    create attribute data type

---
+++ architecture/xml_attribute/xml_attribute.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "xml_attribute.h"
+
+typedef struct xml_attribute {
+};
+
+/* CREATE, INITIALIZE, DESTROY */
+xml_attribute* xml_attribute_create(){
+    xml_attribute* attribute = malloc(sizeof(attribute));
+    return attribute;
+}
+
+xml_attribute* xml_attribute_initialize(xml_attribute* attribute){
+    return attribute;   
+}
+
+void xml_attribute_destroy(xml_attribute* attribute){
+    free(attribute);
+}
+
+/* GETTERS */
+
+
+/* SETTERS */
---
+++ architecture/xml_attribute/xml_attribute.h
@@ -0,0 +1,10 @@
+#ifndef _XML_attribute_H
+#define _XML_attribute_H
+
+extern struct xml_attribute;
+
+xml_attribute* xml_attribute_create();
+xml_attribute* xml_attribute_initialize();
+void xml_attribute_destroy(;
+
+#endif
